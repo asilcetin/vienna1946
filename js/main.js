58be8958d19d7a5fab40631b9c02dfb3242d9893
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2019-02-21 20:01:33 +0100

    side by side layer comparison

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ var mainMap = L.map('mainMap').setView([48.209, 16.369], 13);
 var wmsLayer = L.tileLayer.wms('https://data.wien.gv.at/daten/wms?version=1.1.1', {
   layers: 'BOMBENSCHADENOGD'
 }).addTo(mainMap);
+
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
+}).addTo(mainMap);
+
+L.control.sideBySide(wmsLayer, osmLayer).addTo(mainMap);
